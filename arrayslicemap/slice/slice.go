a81726929b6cdb3574f064c0d63693647d982b65
gabrielaabergamo <gabiibergamo@gmail.com>
2021-11-29 16:16:14 -0300

    slice

--- arrayslicemap/slice/slice.go
+++ arrayslicemap/slice/slice.go
@@ -15,7 +15,7 @@ func main() {
 	a2 := [5]int{1, 2, 3, 4, 5}
 	s2 := a2[1:3] //slice do índice 1 até o 3 sem contar o 3
 	fmt.Println(a2, s2)
-	//slice não gera um array novo! é uma estrutura que tem um ponteiro que aponta para elementos de um determinado array
+	//slice não gera um array novo! é uma estrutura que tem um ponteiro que aponta para um elemento de um determinado array
 	s3 := a2[:2]
 	fmt.Println(a2, s3)
 
