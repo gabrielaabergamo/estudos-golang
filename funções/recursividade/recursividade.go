3c7cfc9c46cda6c77974759e136ef539608433a3
gabrielaabergamo <gabiibergamo@gmail.com>
2021-12-01 10:35:20 -0300

    recursividade

---
+++ funções/recursividade/recursividade.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fatorial(n int) (int, error) {
+	switch {
+	case n < 0:
+		return -1, fmt.Errorf("número inválido: %d", n)
+	case n == 0:
+		return 1, nil
+	default:
+		fatorialAnterior, _ := fatorial(n - 1)
+		return n * fatorialAnterior, nil
+	}
+}
+
+func main() {
+	aux, _ := fatorial(5)
+	fmt.Println(aux)
+
+	_, erro := fatorial(-4)
+	if erro != nil {
+		fmt.Println(erro)
+	}
+}
